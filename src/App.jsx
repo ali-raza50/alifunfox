5430a58f3839f427a73f92ca672c1709b76172e2
ali-raza50 <fa20-bcs-045@cuilahore.edu.pk>
2024-03-31 12:57:49 +0500

    added button fun.add error

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Slide1 from "./components/Slide1";
 import Slide2 from "./components/Slide2";
+import NotFound from "./components/NotFound";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Slide1 />} />
         <Route path="/slide2" element={<Slide2 />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
